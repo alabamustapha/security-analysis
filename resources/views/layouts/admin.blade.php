ac615ed008cb9c44e2661c4d37bf974cd5e44fd2
Alaba mustapha <alabamustapha@gmail.com>
2017-08-27 16:20:10 +0000

    minor fixes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -41,7 +41,7 @@
               <div class="navbar-header">
                 <!-- Navbar Brand --><a href="index.html" class="navbar-brand">
                   <div class="brand-text brand-big hidden-lg-down"><span>Admin</span><strong>Dashboard</strong></div>
-                  <div class="brand-text brand-small"><strong>BD</strong></div></a>
+                  <div class="brand-text brand-small"><strong>SA</strong></div></a>
                 <!-- Toggle Button--><a id="toggle-btn" href="#" class="menu-btn active"><span></span><span></span><span></span></a>
               </div>
               <!-- Navbar Menu -->
